b79f1858dc89af962f8eb387b862c82f90711ca2
harsh-sangwan2003 <hsangwan2002@gmail.com>
2023-12-23 19:07:11 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const userRoute = require('./routes/user.route');
+const authRoute = require('./routes/auth.route');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const connect = async () => {
+
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("DB connection successful.");
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+app.use("/api/user", userRoute);
+app.use("/api/auth",authRoute);
+
+app.listen(5000, () => {
+
+    connect();
+    console.log("Backend server is running.");
+})
