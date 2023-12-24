2e0957bc65b83feb73132624425aa93db028055c
harsh-sangwan2003 <hsangwan2002@gmail.com>
2023-12-24 12:56:43 +0530

    added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -10,6 +10,7 @@ router.post("/register", async (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+        isAdmin:req.body.isAdmin
     })
 
     try {
--- routes/user.route.js
+++ routes/user.route.js
@@ -19,6 +19,22 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     }
 })
 
+// GET ALL USERS
+router.get("/find", verifyTokenAndAdmin, async (req, res) => {
+
+    const query = req.query.new;
+
+    try {
+
+        const users = query ? await User.find().sort({ _id: -1 }).limit(5) : await User.find();
+
+        res.status(200).json(users);
+
+    } catch (error) {
+        res.status(500).json(err);
+    }
+})
+
 
 // UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
